21f5452b9d8860a470300e2487bdb42b34fd2938
Raj Ranjan <rranjan01234@gmail.com>
2023-08-18 15:01:55 +0530

    schema update

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
 create table users (name text, age integer);
 create table keys (name text, key_id text);
 create table payments (name text, amount integer);
-create table new_users (name text, age integer);
